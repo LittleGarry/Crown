df530a92ff56a677c94b31002ef69455fa65d286
LittleGarry <bulgakov_egor@outlook.com>
2019-04-10 14:32:22 +0300

    Generated projects

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,93 @@ workspace "Crown"
 	configurations
 	{
 		"Debug",
-		"Release",
-		"Dist"
+		"Release"
 	}
 
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Crown"
+	location "Crown"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CW_PLATFORM_WINDOWS",
+			"CW_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CW_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "CW_RELEASE"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Crown/src",
+		"Crown/vendor/spdlog/include"
+	}
+
+	links
+	{
+		"Crown"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"CW_PLATFORM_WINDOWS"
+		}
+	
+	filter "configurations:Debug"
+		defines "CW_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "CW_RELEASE"
+		optimize "On"
