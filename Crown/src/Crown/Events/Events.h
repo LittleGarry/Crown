74e72b3260eda2f0b868d96d2f73a25eaa0c7131
LittleGarry <bulgakov_egor@outlook.com>
2019-04-11 16:40:25 +0300

    Added Event class

--- Crown/src/Crown/Events/Events.h
+++ Crown/src/Crown/Events/Events.h
@@ -2,12 +2,47 @@
 
 #include "Crown/Core.h"
 
+#include <string>
+
 namespace Crown {
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication = BIT(0),
+		EventCategoryInput		 = BIT(1),
+		EventCategoryKeyboard	 = BIT(2),
+		EventCategoryMouse		 = BIT(3),
+		EventCategoryMouseButton = BIT(4),
+	};
+
+#define EVENT_CLASS_TYPE(type) virtual EventType GetEventType() const override { return EventType::##type; }\
+							   virtual const char* GetName() const override { return #type; }
 
 	class CROWN_API Event
 	{
 	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetEventCategory() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetEventCategory() & category;
+		}
+
 	protected:
+		bool m_Handled = false;
 	};
 
 }
