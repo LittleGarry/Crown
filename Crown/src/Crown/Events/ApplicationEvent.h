123abe0ac34e626aca3aa237ebf4041a564413ca
LittleGarry <bulgakov_egor@outlook.com>
2019-04-12 12:43:36 +0300

    Added ApplicationEvents

--- Crown/src/Crown/Events/ApplicationEvent.h
+++ Crown/src/Crown/Events/ApplicationEvent.h
@@ -4,6 +4,48 @@
 
 namespace Crown {
 
+	class CROWN_API WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent() {}
 
+		EVENT_CLASS_TYPE(WindowClose)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
 
+	class CROWN_API WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent() {}
+
+		EVENT_CLASS_TYPE(WindowResize)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class CROWN_API AppTickEvent : public Event
+	{
+	public:
+		AppTickEvent() {}
+
+		EVENT_CLASS_TYPE(AppTick)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class CROWN_API AppUpdateEvent : public Event
+	{
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class CROWN_API AppRenderEvent : public Event
+	{
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
 }
