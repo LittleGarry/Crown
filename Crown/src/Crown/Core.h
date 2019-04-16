cbfaa47299769aca8cf59d4c42d6f1ea05f70fff
LittleGarry <bulgakov_egor@outlook.com>
2019-04-16 10:56:28 +0300

    Added asserts

--- Crown/src/Crown/Core.h
+++ Crown/src/Crown/Core.h
@@ -10,4 +10,12 @@
 #error Crown only supports Windows!
 #endif
 
+#ifdef CW_ENABLE_ASSERTS
+	#define CW_ASSERT(x, ...) {if (!(x)) {CW_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); }}
+	#define CW_CORE_ASSERT(x, ...) {if (!(x)) {CW_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak();}}
+#else
+#define CW_ASSERT(x, ...)
+#define CW_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
