ba55c917438802453c10830ac2522db4d218080c
LittleGarry <bulgakov_egor@outlook.com>
2019-04-18 10:47:37 +0300

    Added Dispatch func

--- Crown/src/Crown/Events/Event.h
+++ Crown/src/Crown/Events/Event.h
@@ -56,7 +56,12 @@ namespace Crown {
 		template<typename T>
 		bool Dispatch(std::function<bool(T&)> func)
 		{
-			
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+
+				return true;
+			}
 		}
 
 	private:
